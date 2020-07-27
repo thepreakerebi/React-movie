b8e4249e03d0bdbd9c0573222edf742ac170cfcf
coolPreye <kerebipreye@gmail.com>
2020-07-26 18:40:22 -0800

    first commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { apiKey } from '../util';
+import MovieCard from './MovieCard';
+
+const SearchForm = () => {
+
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async e => {
+        e.preventDefault();
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query.toUpperCase()}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setMovies(data.results);
+        } catch(err) {
+            console.error(err);
+        }
+
+        setQuery('');
+    }
+
+    return (
+        <React.Fragment>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input 
+                    className="input" 
+                    type="text" 
+                    name="query" 
+                    placeholder="i.e Jurassic Park"
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {
+                    movies.filter(movie => movie.poster_path).map(movie => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))
+                }
+            </div>
+        </React.Fragment>
+    );
+} 
+
+export default SearchForm;
