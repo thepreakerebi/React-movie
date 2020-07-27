c9b977e57c5247cf4997d329ee4f5c58f686cfc1
coolPreye <kerebipreye@gmail.com>
2020-07-27 04:21:15 -0800

    Updates

---
+++ precache-manifest.042e94c67da738ab3ec152854fd9aee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d96b1a2b54ce7908cd2a87ab34f53f",
+    "url": "/React-movie/index.html"
+  },
+  {
+    "revision": "051b1750123086bf7943",
+    "url": "/React-movie/static/css/main.a4e3c1a2.chunk.css"
+  },
+  {
+    "revision": "8eb50c975871e0cd539e",
+    "url": "/React-movie/static/js/2.244bd7fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-movie/static/js/2.244bd7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051b1750123086bf7943",
+    "url": "/React-movie/static/js/main.bc8a74a5.chunk.js"
+  },
+  {
+    "revision": "62be6fbfa06a5f598d9e",
+    "url": "/React-movie/static/js/runtime-main.b06520bb.js"
+  }
+]);
